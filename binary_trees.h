2347f4185f64b547d65592773c0d923f1be7a014
willdeng677-pixel <willdeng677@wgmail.com>
2026-08-17 01:29:05 +0000

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -20,5 +20,9 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 #endif /* BINARY_TREES_H */
