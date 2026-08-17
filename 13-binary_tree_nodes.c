1c463a4859497198f0aea66125f677544d7cd594
willdeng677-pixel <willdeng677@gmail.com>
2026-08-17 14:08:52 +0000

    Function that counts the nodes with at least 1 cgild in a binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes with at least 1 child
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Number of nodes with at least 1 child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left)
+			+ binary_tree_nodes(tree->right));
+
+	return (0);
+}
